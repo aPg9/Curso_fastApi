a94538eb5ac8a6270a801e5149534ba167ef7fdd
Adrian Posada <adrian-924@hotmail.com>
2022-06-29 10:23:00 -0500

    Commit de la clase de ejemplos de path y query parameters automaticos

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,10 +1,12 @@
 #Python
+from datetime import date
 from typing import Optional
 from enum import Enum     #-----> Nos ayuda para crear enumeraciones de strings
 
 #Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, PastDate
 from pydantic import Field
+from pydantic.types import PaymentCardBrand, PaymentCardNumber, constr
 
 #FastApi
 from fastapi import FastAPI
@@ -13,6 +15,31 @@ from fastapi import Body, Query, Path
 app = FastAPI()
 
 #Models
+
+class Card(BaseModel):
+    name: constr(strip_whitespace=True, min_length=1)
+    number: PaymentCardNumber
+    exp: date
+
+    @property
+    def brand(self) -> PaymentCardBrand:
+        return self.number.brand
+
+    @property
+    def expired(self) -> bool:
+        return self.exp < date.today()
+
+
+card = Card(
+    name='Georg Wilhelm Friedrich Hegel',
+    number='4000000000000002',
+    exp=date(2023, 9, 30),
+)
+
+assert card.number.brand == PaymentCardBrand.visa
+assert card.number.bin == '400000'
+assert card.number.last4 == '0002'
+assert card.number.masked == '400000******0002'
 
 class HairColor(Enum):
     white = "white"
@@ -48,12 +75,14 @@ class Person(BaseModel):
         min_length= 1,
         max_length=50,
         example = "Rogelio"
-        )
+    )
+
     last_name: str = Field(
         min_length= 1,
         max_length=50,
         example = "Juarez"
     )
+
     age: int = Field(
         ...,
         gt=0,
@@ -61,9 +90,24 @@ class Person(BaseModel):
         example = "45"
     )
 
+    birth_day: PastDate = Field(
+        ...,
+        example = "2002-11-24"
+    )
+
+
+    email: EmailStr = Field(
+        ...,
+        exmaple= "prueba@pruebaemail.com"
+    )
+
+    card: Card = Field(
+        ...,
+    )
+
     hair_color: Optional[HairColor] = Field(
-    default=None,
-    example= "Black"
+        default=None,
+        example= "Black"
     )
     is_married: Optional[bool] = Field(
         default=None,
@@ -80,7 +124,7 @@ class Person(BaseModel):
     #            "is_married": False,
     #        }
     #    }
-    
+
 
 @app.get("/")     #-----> Path opetation decorator
 def home():
